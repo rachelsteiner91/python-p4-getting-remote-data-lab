9cede4b2bc2a06e7499f0edbbff73274c375c603
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-06-13 15:56:32 -0400

    getting remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
 import json
 
+
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+    ##module get_response_body function returns response.
+        response = requests.get(self.url)
+        return response.content
 
+    ##module load_json function returns response.
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
